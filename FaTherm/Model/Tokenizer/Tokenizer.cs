8e2d25d8ed39de5ea2b0e2b59905e5da31f9a4d8
Attila Farkas <farkas_ati@yahoo.com>
2014-05-28 08:14:59 +0300

    Partial changes due to the laptop's imminent death. It barely compiles.

--- FaTherm/Model/Tokenizer/Tokenizer.cs
+++ FaTherm/Model/Tokenizer/Tokenizer.cs
@@ -9,29 +9,52 @@ namespace FaTherm.Model.Tokenizer
     class Tokenizer<TokensEnum> where TokensEnum : IConvertible
     {
         private DeterministicFiniteAutomaton Automaton = null;
-        //private Enum Tokens = null;
 
         private string _stringToTokenize = null;
         private int _currentPositionInString = 0;
+        private int _oldstate = -1;
 
         public Tokenizer(DeterministicFiniteAutomaton automaton/*, Enum tokens*/)
         {
             Automaton = automaton;
-            //Tokens = tokens;
+
+            if (!typeof(TokensEnum).IsEnum)
+                throw new ArgumentException("TokensEnum must be an enumerated type.");
         }
 
-        private int GetNextToken(ref Enum tokenType, ref string tokenValue)
+        public void SetStringToTokenize(string s)
         {
-            if (_stringToTokenize == null || _stringToTokenize.Equals(""))
-                return 0;
+            _stringToTokenize = s;
+            _currentPositionInString = 0;
+            _oldstate = -1;
+        }
 
-            int oldState = Automaton.ReadCharacter(_stringToTokenize[_currentPositionInString++]), currentState = oldState;
+        private bool GetNextToken(ref TokensEnum tokenType, ref string tokenValue)
+        {
+            // If we have an empty string return the default values.
+            if (_stringToTokenize == null || _stringToTokenize.Equals(""))
+            {
+                tokenType = (TokensEnum)(object)0;
+                tokenValue = "";
+                return true;
+            }
+
+            int oldState = Automaton.ReadCharacter(_stringToTokenize[_currentPositionInString++]), 
+                currentState = oldState;
+
+            // If the first character wasn't processed successfully the tokenization failed.
+            if (oldState == -1)
+                return false;
 
             while (oldState == currentState)
             {
+                currentState = Automaton.ReadCharacter(_stringToTokenize[_currentPositionInString++]);
             }
 
-            return 0;
+            tokenType = (TokensEnum)(object)oldState;
+            //tokenType = Enum.ToObject(typeof(TokensEnum) , oldState));
+
+            return true;
         }
 
         public List<Enum> GetTokens()
